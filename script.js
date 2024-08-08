fb252937a966cfb55c8a325fa6a3f299eef18e22
Thalia <148969711+TZevs@users.noreply.github.com>
2024-08-08 15:45:37 +0100

    Responsive navbar complete.

---
+++ script.js
@@ -0,0 +1,32 @@
+// Mobile Navigation 
+const menu = document.querySelector(".navbar");
+const menuItems = document.querySelectorAll(".nav-item");
+const hamburger = document.querySelector("#menu-icon");
+const body = document.querySelector("body");
+
+function toggleMenu() {
+    if (menu.classList.contains("showNavbar")) {
+        menu.classList.remove("showNavbar");
+    } else {
+        menu.classList.add("showNavbar");
+    }
+}
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach(
+    function(menuItem) {
+        menuItem.addEventListener("click", function() {
+            menu.classList.remove("showNavbar");
+        })
+    }
+)
+
+document.addEventListener("click", function(ev) {
+    const isClickInsideMenu = menu.contains(ev.target);
+    const isClickOutsideMenu = hamburger.contains(ev.target);
+
+    if (!isClickInsideMenu && !isClickOutsideMenu && menu.classList.contains("showNavbar")) {
+        menu.classList.remove("showNavbar");
+    }
+})
+
